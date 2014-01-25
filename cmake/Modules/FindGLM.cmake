90ce6fd770e31132ac5bd50379c6b0dfc60fd62f
Leon Lynch <lynch.leon@gmail.com>
2014-01-25 13:54:09 +0200

    Improve CMake module for GLM
    * Parse version number from setup.hpp
    * Document output variables
    * Update advanced variables

--- cmake/Modules/FindGLM.cmake
+++ cmake/Modules/FindGLM.cmake
@@ -5,12 +5,33 @@
 # See LICENSE file.
 ##############################################################################
 
+# This will define:
+#
+# GLM_FOUND
+# GLM_INCLUDE_DIR
+# GLM_VERSION
+
 find_path(GLM_INCLUDE_DIR "glm/glm.hpp")
+find_file(GLM_SETUP_HPP NAMES "glm/core/setup.hpp" "glm/detail/setup.hpp")
+
+if(GLM_SETUP_HPP)
+	file(STRINGS
+		${GLM_SETUP_HPP}
+		GLM_VERSION_DEFINES
+		REGEX "#define[ \t]+GLM_VERSION_[A-Z]+[ \t]+[0-9]+$"
+	)
+	if(GLM_VERSION_DEFINES)
+		foreach(item IN ITEMS MAJOR MINOR PATCH REVISION)
+			string(REGEX REPLACE ".*#define[ \t]+GLM_VERSION_${item}[ \t]+([0-9]+).*" "\\1" GLM_VERSION_${item} ${GLM_VERSION_DEFINES})
+		endforeach()
+	endif()
+	set(GLM_VERSION "${GLM_VERSION_MAJOR}.${GLM_VERSION_MINOR}.${GLM_VERSION_PATCH}.${GLM_VERSION_REVISION}")
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLM
-	REQUIRED_VARS GLM_INCLUDE_DIR
-	FAIL_MESSAGE "Failed to find GLM header"
+	REQUIRED_VARS GLM_INCLUDE_DIR GLM_SETUP_HPP
+	VERSION_VAR GLM_VERSION
 )
 
-mark_as_advanced(GLM_INCLUDE_DIR)
+mark_as_advanced(GLM_FOUND GLM_INCLUDE_DIR GLM_SETUP_HPP GLM_VERSION_MAJOR GLM_VERSION_MINOR GLM_VERSION_PATCH GLM_VERSION_REVISION GLM_VERSION)
